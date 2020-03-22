f963025b6019d50fd9cb8b9b835b1e9a79d9b9bf
cheun0327 <48849608+cheun0327@users.noreply.github.com>
2020-03-22 23:27:36 +0900

    todo

--- src/js/main.js
+++ src/js/main.js
@@ -3,6 +3,21 @@ import '../scss/main.scss';
 const X_IMAGE = "/x.93083e62.svg";
 
 /*
+강의 내용
+1) querySelector, querySelectorAll
+2) addEventListener
+3) classList.add, remove
+4) parentNode
+5) createElement
+6) appendChild
+7) DOM.style
+8) confirm
+9) focus
+10) localStorage
+11) JSON.parse, JSON.stringify
+*/
+
+/*
 1. Card Delete
  - X 버튼을 눌렀을 때, 카드가 삭제됨.
 
@@ -26,7 +41,6 @@ const X_IMAGE = "/x.93083e62.svg";
  - 카테고리 이름을 비웠을 때, 카테고리 삭제
  - 확인 메세지를 통해 삭제 여부 확인
 
- 
 8. LocalStorage Load
  - 로컬스토리지에서 투두리스트 불러오기
  - example) {"개발":[{"title":"자스민 2주차 과제","description":"노마드 코더 메인페이지 클로닝 하기"},{"title":"자스민 1주차 과제","description":"유튜브 메인페이지 클로닝 하기"}],"집안일":[{"title":"장보기","description":"세제, 치약 사기"},{"title":"설거지","description":"라면 포트 설거지하기"},{"title":"청소","description":"화장실 청소하기"}],"업무":[{"title":"프론트 작업","description":"모바일 프론트 작업 및 리팩토링"}]}
@@ -42,53 +56,87 @@ const X_IMAGE = "/x.93083e62.svg";
 */
 
 window.onload = () => {
-    const cardDeleteBtn = document.querySelectorAll(".card .delete-btn");/*DOM선택*/
-    console.log(cardDeleteBtn)
-    cardDeleteBtn.forEach(cardDeleteBtn => {
-        /*클릭했을때 원소 삭제 이벤트*/
-        cardDeleteBtn.addEventListener("click", cardDeleteHandler)
-    })
+    /* 1. Card Delete */
+    // const cardDeleteBtns = document.querySelectorAll(".card .delete-btn");
+    // cardDeleteBtns.forEach(cardDeleteBtn => {
+    //     cardDeleteBtn.addEventListener("click", deleteCardHandler);
+    // })
 
-    const categoryAddBtn = document.querySelector(".category-add-btn")/* 버튼 하나만 존재해서 all안붙임 */
-    categoryAddBtn.addEventListener("click", categoryAddHandler);
+    /* 2. Category Create */
+    const categoryAddBtn = document.querySelector(".category-add-btn");
+    categoryAddBtn.addEventListener("click", addCategoryHandler);
 
-    const cardAddBtns = document.querySelectorAll(".column .add-btn");
-    cardAddBtns.forEach(cardAddBtn => {
-        cardAddBtn.addEventListener("click", showModalHandler);
-    })
+    /* 3. Modal Show */
+    // const cardAddBtns = document.querySelectorAll(".todo-category .add-btn");
+    // cardAddBtns.forEach(cardAddBtn => {
+    //     cardAddBtn.addEventListener("click", showModalHandler);
+    // })
 
-    const cancelBtn = document.querySelector(".modal .cancel-btn");
-    cancelBtn.addEventListener("click", closeModalHandler);
+    /* 4. Modal Close */
+    const modalCloseBtn = document.querySelector(".modal .cancel-btn");
+    modalCloseBtn.addEventListener("click", closeModalHandler);
 
+    /* 6. Card Create */
     const cardAddBtn = document.querySelector(".modal .save-btn");
-    cardAddBtn = addEventListener("click", cardAddHandler);
-    
-    const categoryInputs = document.querySelectorAll(".column .category");
-    categoryInputs.forEach(categoryInput => {
-        categoryInput.addEventListener("change", changeCategoryHandler);
-    })
+    cardAddBtn.addEventListener("click", cardAddHandler);
 
+    /* 7. Category Delete */
+    // const categoryInputs = document.querySelectorAll(".todo-category .category");
+    // categoryInputs.forEach(categoryInput => {
+    //     categoryInput.addEventListener("change", changeCategoryHandler);
+    // })
+
+    /* 8. LocalStorage Load */
     loadLocalStorage();
+
+    /* 11. Auto Increse Textarea */
+    const descriptions = document.querySelectorAll(".card .description");
+    descriptions.forEach(description => {
+        description.style.height = (description.scrollHeight) + "px";
+    })
 }
 
-/* 1 (삭제) */
+/* 1. Delete Card */
 const findTargetClass = (node, targetClass) => {
-    /*노드를 가지고 있는지 판단*/
-    while(node.classList.contains(targetClass) == false){
+    while (node.classList.contains(targetClass) == false)
         node = node.parentNode;
-    }
     return node;
 }
 
-const cardDeleteHandler = (event) => {
-    const card = findTargetClass(event.target, "card");
-    card.remove();
+const deleteCardHandler = (event) => {
+    var target = findTargetClass(event.target, "card");
+    target.remove();
+    /* 10. LocalStorage Update */
+    setLocalStorage();
 }
-/* end 1 */
+/* end: 1 */
 
-/* 2 (카테고리 추가) */
+/* 2. Category Create */
+const addCategoryHandler = () => {
+    const categoryTitle = document.querySelector(".category-title");
+    if (categoryTitle.value == "") {
+        alert("카테고리 이름을 작성해주세요.");
+        return;
+    }
+
+    const categories = document.querySelectorAll(".category");
+    for (var i = 0; i < categories.length; i++) {
+        if (categories[i].value == categoryTitle.value) {
+            alert("이미 존재하는 카테고리입니다.");
+            return;
+        }
+    }
+
+    const column = createColumn(categoryTitle.value);
+    const todoContainer = document.querySelector(".todo-container");
+    todoContainer.appendChild(column);
+
+    categoryTitle.value = "";
+    /* 10. LocalStorage Update */
+    setLocalStorage();
+}
+
 const createColumn = (categoryTitle) => {
-    //valina js로 만들어주기
     const column = document.createElement("div");
     column.classList.add("column");
 
@@ -98,116 +146,116 @@ const createColumn = (categoryTitle) => {
     const categoryInput = document.createElement("input");
     categoryInput.classList.add("category");
     categoryInput.value = categoryTitle;
+    /* 9. Event Check */
+    categoryInput.addEventListener("change", changeCategoryHandler);
 
-    const addBt = document.createElement("img");
-    addBt.classList.add("add-btn");
-    addBt.src = X_IMAGE;
+    const addBtn = document.createElement("img");
+    addBtn.classList.add("add-btn");
+    addBtn.src = X_IMAGE;
+    /* 9. Event Check */
+    addBtn.addEventListener("click", showModalHandler);
 
-    //합쳐주는 작업
     todoCategory.appendChild(categoryInput);
-    todoCategory.appendChild(addBt);
+    todoCategory.appendChild(addBtn);
     column.appendChild(todoCategory);
 
     return column;
 }
+/* end: 2 */
 
-const categoryAddHandler = () => {
-    //(1) input 값을 가져오기
-    const categoryTitle = document.querySelector("input.category-title");//input태으이면서 cartegory-title인
-    console.log(categoryTitle.value);
-
-    //(2) 값 검증 : 이름 없을 때, 중복되는 이름 있을때
-    if(categoryTitle.value == ""){
-        alert("카테고리 이름을 작성해주세요.");
-        return;
-    }
-
-    const categories = document.querySelectorAll(".column .category");
-    for(var i = 0; i < categories.length; i++){
-        if(categories[i].value == categoryTitle.value){
-            alert("이미 존재하는 카테고리입니다.");
-            return;
-        }
-    }
-
-    //(3) 컬럼 만들기
-    const column = createColumn(categoryTitle.value);
-    const todoContainer = document.querySelector(".todo-container");
-    todoContainer.appendChild(column);
-
-}
-/* end 2 */
-
-/* 5 */
-const findCategory = (event) => {
-    //DOM객체 안에서 querySelector사용하면 DOM객체 안의 것만 return
-    const currentInput = event.target.parentNode.querySelector(".category");
-    return currentInput.value;
-}
-
-const updateSelectbox = (event) => {
-    const selectBox = document.querySelector(".modal select");
-    selectBox.innerText = "";
-    
-    const categories = document.querySelectorAll(".column .category");
-    categories.forEach(category => {
-        const categoryOption = document.createElement("option");
-        categoryOption.innerText = category.value;
-        selectBox.appendChild(categoryOption);
-    });
-
-    //카테고리 이름을 클릭한 카테고리로 바꾸기
-    const currentCategory = findCategory(event);
-    selectBox.value = currentCategory;
-}
-/* end 5 */
-
-/* 3 */
+/* 3. Modal Show */
 const showModalHandler = (event) => {
     const modalContainer = document.querySelector(".modal-container");
     const body = document.querySelector("body");
-    console.log(modalContainer);
 
-    //css에 display none걸어준거 삭제
+    modalContainer.style.top = `${window.scrollY}px`;
     modalContainer.classList.remove("dp-none");
-    //스크롤 삭제, 원래 스크롤 위치에서 모달 보여줌
-    modalContainer.style.top = '${window.scrollY}px';
     body.classList.add("stop-scroll");
 
-    updateSelectbox(event);
-};
+    /* 5. Options Create */
+    updateSelectBox(event);
+}
+/* end: 3 */
 
-/* 4 */
-const closeModalHandler = () =>{
+/* 4. Modal Close */
+const closeModalHandler = () => {
     const modalContainer = document.querySelector(".modal-container");
     const body = document.querySelector("body");
 
-    //css에 display none걸어준거 복구
     modalContainer.classList.add("dp-none");
-    //스크롤 복구
     body.classList.remove("stop-scroll");
 }
-/* end 4 */
+/* end:4 */
 
-/* 6 */ //저장버튼 안눌림!!!!
-const createCard = (titleValue, DescValue) => {
+/* 5. Options Create */
+const updateSelectBox = event => {
+    const selectBox = document.querySelector(".modal select");
+    selectBox.innerText = "";
+
+    const categoryInputs = document.querySelectorAll(".category");
+    categoryInputs.forEach(categoryInput => {
+        const categoryOption = document.createElement("option");
+        categoryOption.innerText = categoryInput.value;
+        //console.log(categoryOption);
+        selectBox.appendChild(categoryOption);
+    })
+
+    const currentCategory = findCategory(event);
+    selectBox.value = currentCategory;
+}
+
+const findCategory = event => {
+    const currentInput = event.target.parentNode.querySelector(".category");
+    return currentInput.value;
+}
+/* end: 5 */
+
+/* 6. Card Create */
+const cardAddHandler = () => {
+    const categorySelect = document.querySelector(".modal select");
+    const cardTitleInput = document.querySelector(".modal input");
+    const cardDescTextarea = document.querySelector(".modal textarea");
+
+    const card = createCard(cardTitleInput.value, cardDescTextarea.value);
+    //console.log(card);
+
+    addToCategory(categorySelect.value, card);
+    closeModalHandler();
+
+    categorySelect.value = "";
+    cardTitleInput.value = "";
+    cardDescTextarea.value = "";
+
+    /* 10. LocalStorage Update */
+    setLocalStorage();
+}
+
+const createCard = (titleValue, descriptionValue) => {
     const card = document.createElement("div");
     card.classList.add("card");
 
-    const deleteBtn = document.createElement("img");
+    var deleteBtn = document.createElement("img");
     deleteBtn.classList.add("delete-btn");
     deleteBtn.src = X_IMAGE;
+    /* 9. Event Check */
+    deleteBtn.addEventListener("click", deleteCardHandler);
 
     const cardTitle = document.createElement("input");
-    cardTitle.classList.add("title");
+    cardTitle.classList.add("title")
     cardTitle.value = titleValue;
+    /* 10. LocalStorage Update */
+    cardTitle.addEventListener("change", setLocalStorage);
 
     const divider = document.createElement("div");
-    deleteBtn.classList.add("divider");
+    divider.classList.add("divider");
 
     const cardDescription = document.createElement("textarea");
     cardDescription.classList.add("description");
-    cardDescription.value = DescValue;
+    cardDescription.value = descriptionValue;
+    /* 10. LocalStorage Update */
+    cardDescription.addEventListener("change", setLocalStorage);
+    /* 11. Auto Increase Textarea */
+    cardDescription.addEventListener("input", autoTextarea);
 
     card.appendChild(deleteBtn);
     card.appendChild(cardTitle);
@@ -217,70 +265,80 @@ const createCard = (titleValue, DescValue) => {
     return card;
 }
 
-const addToCategory = (categoryValue, card) =>{
+const addToCategory = (targetCategory, card) => {
     const columns = document.querySelectorAll(".column");
 
-    for(var i = 0; i < columns.length; i++){
+    for (var i = 0; i < columns.length; i++) {
         const category = columns[i].querySelector(".category").value;
-        if(categoryValue == category){
-            columns[i].appendChild(category);
+        if (targetCategory == category) {
+            columns[i].appendChild(card);
             break;
         }
     }
 }
 
-const cardAddHandler = () => {
-    const selectBox = document.querySelector("modal select");
-    const cardTitleInput = document.querySelector("modal input");
-    const cardDescTextarea = document.querySelector("modal textarea");
-
-    const card = createCard(cardTitleInput.value, cardDescTextarea.value);
-
-    addToCategory(selectBox.value, card);
-    closeModalHandler();
-
-    selectBox.value = "";
-    cardTitleInput.value = "";
-    cardDescTextarea.value = "";
-
+/* 11. Auto Increase Textarea */
+const autoTextarea = (event) => {
+    event.target.style.height = (event.target.scrollHeight) + "px";
 }
-/* end 6 */
+/* end: 11 */
 
+/* end: 6 */
 
-/* 7 */
-const changeCategoryHandler = (event) => {
-    if(event.target.value == "") {
-        const deleteConfirm = confirm("정말 카테고리를 삭제하시겠습니까???"); //확인 = return true, 취소 = return false
-
-        if(deleteConfirm) {
-            const column = findTargetClass(event.target, "column");
+/* 7. Category Delete */
+const changeCategoryHandler = event => {
+    if (event.target.value == "") {
+        const deleteConfirm = confirm("정말 카테고리를 삭제하시겠습니까?")
+        if (deleteConfirm) {
+            var column = findTargetClass(event.target, "column");
             column.remove();
-        }else{
-            event.target.focus(); //커서가 남는다.
+        } else {
+            event.target.focus();
         }
     }
+    /* 10. LocalStorage Update */
+    setLocalStorage();
 }
-/* end 7 */
+/* end: 7 */
 
-/*8 페이지 저장하기*/
+/* 8. LocalStorage Load */
 const loadLocalStorage = () => {
-    var todoLists = localStorage.TODO_LIST;
-    todoLists = JSON.parse(todoLists);
+    var todoLists = localStorage.TODO_LISTS;
+    if (todoLists) todoLists = JSON.parse(todoLists);
 
-    todocontainer = document.querySelector(".todo-container");
-
-    for(var category in todoList) {
-        console.log(category);
-        console.log(todoLists[category]);
-
+    for (var category in todoLists) {
         const todoList = todoLists[category];
 
         const column = createColumn(category);
         todoList.forEach(todo => {
-            const card = createCard(todo.title, todo.description);
+            const card = createCard(todo['title'], todo['description']);
             column.appendChild(card);
-        })
+        });
+
+        const todoContainer = document.querySelector(".todo-container");
         todoContainer.appendChild(column);
     }
 }
-/*end 8*/
+/* end: 8 */
+
+/* 10. LocalStorage Update */
+const setLocalStorage = () => {
+    var todoLists = {};
+    const columns = document.querySelectorAll(".column");
+
+    columns.forEach(column => {
+        const cateogoryTitle = column.querySelector(".category").value;
+        const cards = column.querySelectorAll(".card");
+
+        var todoList = [];
+        cards.forEach(card => {
+            const title = card.querySelector(".title").value;
+            const description = card.querySelector(".description").value;
+
+            todoList.push({ title, description });
+        })
+        todoLists[cateogoryTitle] = todoList;
+    });
+    localStorage.TODO_LISTS = JSON.stringify(todoLists);
+}
+/* end: 10 */
